a477bbecda5058029ababdd2133f324de3e2b88d
Adelires Chelepa <adelires.chelepa@compasso.com.br>
2022-08-21 16:59:22 -0300

    :tada: challenge finished

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,25 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+      <ModalOverlay />
+      <ModalContent m="auto" maxW="900px" maxH="600px">
+        <ModalBody p="0">
+          <Image src={imgUrl} maxW="900px" maxH="600px" />
+        </ModalBody>
+        <ModalFooter h="32px" bgColor="pGray.800" borderEndEndRadius="6px">
+          <Link
+            href={imgUrl}
+            isExternal
+            fontSize="14px"
+            mr="auto"
+            color="pGray.50"
+          >
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
